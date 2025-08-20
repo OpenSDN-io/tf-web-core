a2ef7d23407c2248a825580e75be8b481872413e
tikitavi <rtikitavi@gmail.com>
2025-08-20 14:08:28 +0300

    trying to fix webui in rocky9
    Depends-on: I1aaafa2f2d749d6e22c71276f56166d09afbb3a6
    Change-Id: I746896435bb05e4929cebf99e43011d466dc7278
    Signed-off-by: tikitavi <rtikitavi@gmail.com>

--- src/serverroot/common/threads.api.js
+++ src/serverroot/common/threads.api.js
@@ -1,7 +1,16 @@
 /*
  * Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
  */
-var Threads = require("webworker-threads");
+let Threads;
+let isModern = false;
+try {
+    // try to use the worker_threads module available in Node.js 10.5.0 and later
+    Threads = require('worker_threads');
+    isModern = true;
+} catch (e) {
+    // Fallback to the webworker-threads module for older Node.js versions
+    Threads = require('webworker-threads');
+}
 
 /* Function: createWorkerThread
  * public function
@@ -29,5 +38,46 @@ function runInThread (threadCB, data, callback) {
     );
 }
 
-exports.runInThread = runInThread;
+function runInThreadModern(threadCB, data, callback) {
+    const { Worker } = Threads;
 
+    // Create worker code as a string. It will run in a separate thread.
+    const workerCode = `
+        const { parentPort, workerData } = require('node:worker_threads');
+        // Restore the function that was passed from the main thread
+        const fn = (${threadCB.toString()});
+        // Execute the function with given data
+        fn(workerData, (err, res) => {
+            // Send result back to the main thread
+            parentPort.postMessage({ err: err ? String(err) : null, res });
+        });
+    `;
+
+    // Create a new worker from this code (eval means code is given as string)
+    const worker = new Worker(workerCode, {
+        eval: true,
+        workerData: data,  // data is passed to workerData inside worker
+    });
+
+    // Listen for the result message from the worker
+    worker.on('message', (msg) => {
+        // Call the callback provided by the caller
+        callback(msg.err ? new Error(msg.err) : null, msg.res);
+        // Destroy worker after the work is done
+        worker.terminate();
+    });
+
+    // If worker fails to start or throws error, report it
+    worker.on('error', (err) => {
+        callback(err);
+    });
+
+    // If worker exits abnormally, report it too
+    worker.on('exit', (code) => {
+        if (code !== 0) {
+            callback(new Error('Worker stopped with exit code ' + code));
+        }
+    });
+}
+
+exports.runInThread = isModern ? runInThreadModern : runInThread;
